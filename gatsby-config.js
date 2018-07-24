0d7dae74b970ced35c09cf1fae28d5f44df70aef
Paul Clifford <clifford@queensu.ca>
2018-07-24 10:55:22 -0400

    removed typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,11 +16,5 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
   ],
 }
