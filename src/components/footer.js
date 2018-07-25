7464961fe83bb472fb8ebe977e5c096b77f6c881
Paul Clifford <clifford@queensu.ca>
2018-07-25 10:13:49 -0400

    updated styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,10 @@ const Footer = () => (
     <a href="https://library.queensu.ca">
       <img id="qul-logo" src={QULlogo} alt="Queen's University Library"/>
     </a>
+    <aside class="site-contact">
+      <h3>Contact Us</h3>
+      <p>(613) 234-1234</p>
+    </aside>
   </footer>
 )
 
