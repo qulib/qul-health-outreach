03662d8527145e8af1daa45ab07c09298b018cda
Paul Clifford <clifford@queensu.ca>
2018-07-26 13:53:53 -0400

    Post category filtering working in GraphQL!

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,13 @@ import Card from "../components/card"
 
 class Home extends Component {
   render() {
-    const data = this.props.data
-
-    console.log(data);
+    const data = this.props.data;
 
     return (
       <Layout>
         <div className="cards">
-
           {data.allWordpressPost.edges.map(({ node }) => (
             <div key={node.slug}>
-
-              <p>hi: {node.categories.slug}</p>
-
               <Card 
                 image={node.featured_media.localFile.childImageSharp.resize.src}
                 image_alt={node.featured_media.alt_text}
@@ -26,10 +20,7 @@ class Home extends Component {
                 content={node.content} 
                 link={node.acf.link}
               />
-
-
             </div>
-            
           ))}
         </div>
       </Layout>
@@ -42,12 +33,9 @@ export default Home
 // Set here the ID of the home page.
 export const pageQuery = graphql`
 {
-  allWordpressPost(sort: {fields: [date]}) {
+  allWordpressPost(sort: {fields: [date]}, filter: {categories: {slug: {eq: "cat-1"}}}) {
     edges {
       node {
-        categories {
-          slug
-        }
         title
         slug
         content
@@ -68,5 +56,4 @@ export const pageQuery = graphql`
     }
   }
 }
-
 `
