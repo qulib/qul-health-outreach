d956bcde00aa10f3702dcf184bc13539c5b05f27
Paul Clifford <clifford@queensu.ca>
2021-04-07 11:58:22 -0400

    node updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,46 @@
 module.exports = {
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "Health Outreach Partners",
-        short_name: "Health Outreach Partners",
-        start_url: "/",
-        background_color: "#406486",
-        theme_color: "#00305e",
-        display: "minimal-ui",
-        icon: "src/images/qul-squares.png", 
-      }
+        name: 'Health Outreach Partners',
+        short_name: 'Health Outreach Partners',
+        start_url: '/',
+        background_color: '#406486',
+        theme_color: '#00305e',
+        display: 'minimal-ui',
+        icon: 'src/images/qul-squares.png',
+      },
     },
     {
-      resolve: "gatsby-source-wordpress",
+      resolve: 'gatsby-source-wordpress',
       options: {
-        baseUrl: "sites.library.queensu.ca/health-outreach",
-        protocol: "https",
+        baseUrl: 'sites.library.queensu.ca/health-outreach',
+        protocol: 'https',
         hostingWPCOM: false,
         useACF: true,
         verboseOutput: false,
         excludedRoutes: [
-          "/akismet/**",
-          "/jetpack/**",
-          "/wp/v2/jp_pay_order/**",
-          "/wp/v2/users/**"
-        ]
-      }
+          '/akismet/**',
+          '/jetpack/**',
+          '/wp/v2/jp_pay_order/**',
+          '/wp/v2/users/**',
+        ],
+      },
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-88346038-1",
+        trackingId: 'UA-88346038-1',
         head: false,
         anonymize: true,
-        respectDNT: true 
-      }
+        respectDNT: true,
+      },
     },
-    "gatsby-plugin-offline"
-  ]
+    'gatsby-plugin-offline',
+  ],
 }
